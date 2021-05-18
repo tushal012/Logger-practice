01a7bcf1de89084b1ce831cd3865389d0c9cd1e0
tushal012 <tushalkashyap11@gmail.com>
2021-05-18 16:25:23 +0530

    added logging in app class

--- src/main/java/com/bridgeLabz/logger/App.java
+++ src/main/java/com/bridgeLabz/logger/App.java
@@ -1,13 +1,32 @@
 package com.bridgeLabz.logger;
 
+import java.io.IOException;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.HTMLLayout;
+import org.apache.log4j.Layout;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.SimpleLayout;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+	 static Logger logger = Logger.getLogger(App.class);
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        Layout layout = new PatternLayout("%d %M %C %m %n");
+        Appender appender = new FileAppender(layout, "Applog.log");
+        logger.addAppender(appender);
+        logger.debug("debug priority");
+        logger.info("info priority");
+        logger.warn("warn priority");
+        logger.error("error priority");
+        logger.fatal("free priority");
     }
 }
